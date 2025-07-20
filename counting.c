3f7d3641c23cd17bdc2dde7010d64d555753d152
Ronak De <ronakde84@gmail.com>
2025-07-20 19:33:07 +0100

    Update counting.c

--- counting.c
+++ counting.c
@@ -29,33 +29,40 @@ int max_finder(int* arr, int size){
 }
 void radixsort(int* arr, int size){
     int max_val = max_finder(arr, size);
-    int n_iterations = ceil(log10(max_val))+1;
+    int n_iterations = ceil(log10(max_val));
     for (int pos = 0; pos <= n_iterations; pos ++){
-        int **intermediate = (int **)malloc(size * sizeof(int *));
-        for (int i = 0; i < size; ++i) {
-            intermediate[i] = (int *)calloc(10, sizeof(int));
+        int **intermediate = (int **)malloc(10 * sizeof(int *));
+        for (int i = 0; i < 10; ++i) {
+            intermediate[i] = (int *)calloc(size, sizeof(int));
         }
 
         int posarr[10] = {0};
         radix_pair *arr_pair = (radix_pair *)calloc(size, sizeof(radix_pair));
         for (int i = 0; i < size; i++){
-            //pairlst[i] = pos_generator(intlst[i],0);
             arr_pair[i] = pos_generator(arr[i],pos);
         }
 
         for (int i = 0; i < size; i++) {
             int digit = arr_pair[i].x;
-            intermediate[digit][posarr[digit]++] = arr_pair[i].n;
+            intermediate[posarr[digit]++][digit] = arr_pair[i].n;
         }
-        //rebuild: start with intermediate[0], list all then go to next etc. the number of items given by posarr[i]
+        printf("hi");
+        for (int i = 0; i < size; i++){
+            printall(intermediate[i],10);
+        }
+        printf("\n");
+
         int index = 0;
         for (int i = 0; i < 10; i++){
             for (int j = 0; j < posarr[i]; j++){
                 arr[index++] = intermediate[i][j];
             }
         }
+
+        free(arr_pair);
+        free(intermediate);
         printall(arr,size);
-    }    
+    }
 }
 
 
@@ -84,7 +91,7 @@ void countingsort(int* arr, int size){
 
 int main(){
     printf("hello world \n");
-    int arr[] = {1,2,4,25,34,16};
+    int arr[] = {1,2,4,25,34,16,19};
     int size = sizeof(arr)/sizeof(int);
 
     printall(arr,size);
